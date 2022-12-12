0378823eb54a6b0b7b6b9359319fd965a1f44540
Jair Orozco <dg.jairsanmartin@gmail.com>
2022-12-12 11:53:05 -0600

    Creación de componente que se muestra cuando no hay ninguna tarea en el localStorage. Muestra el mensaje de no hay tareas pendientes.

---
+++ src/components/NoTaskPending.jsx
@@ -0,0 +1,7 @@
+export const NoTaskPending = () => {
+    return (
+        <section className="emptyTodos">
+            <p className="textEmptyTodos"> You don´t have any task pending </p>
+        </section>
+    )
+}
